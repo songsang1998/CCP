4b729e1555e6e1abadee0b2217fad935088030a9
songsang1998 <44784597+songsang1998@users.noreply.github.com>
2018-11-15 23:21:25 +0900

    Update1 manager.cpp
    manger.cpp 영어제거

--- manager.cpp
+++ manager.cpp
@@ -4,25 +4,25 @@
 using namespace std;
 
 int Password_Setting(string &d) {
-	//비밀번호 재설정
-	return 0; //일단은
+	
+	return 0; 
 }
 int Cafe_Management() {
-	//카페 관리
-	return 0; //일단은
+	
+	return 0; 
 }
 
 int Cat_Management() {
-	//고양이 관리
-	return 0; //일단은
+	
+	return 0; 
 }
 
 
 bool manager_Interface(string &c){
 	while (1)
 	{
-		cout << "관리자모드에 오신것을 환영합니다." << endl << "아래 번호중 하나를 선택하세요." << endl;
-		cout << "1.암호설정" << endl << "2.카페관리" << endl << "3.고양이 먹이";
+		cout << "Welcome to Manager Mode." << endl << "Please select one of the numbers." << endl;
+		cout << "1.Password Settings" << endl << "2.Cafe management" << endl << "3.Cat management"<<endl;
 		char b;
 		cin >> b;
 		switch (b) {
