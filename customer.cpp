e6d407dda57795bf0599a72cf403d31173b839fd
junha-lee <44857783+junha-lee@users.noreply.github.com>
2018-11-15 23:04:09 +0900

    update customer.cpp
    한국어제거

--- customer.cpp
+++ customer.cpp
@@ -2,16 +2,13 @@
 
 
 int admission_fee(int &r) {
-	//인원수를 입력받아서 요금에 입장료*인원수를 합친후 추가주문 선택
-	return 0; //일단은
+		return 0;
 }
 int coffee(int &r) {
-	//커피 종류 띄어줌 주문,HoT/ICE,갯수후 추가주문 선택, 재료 소진
-	return 0; //일단은
+		return 0; 
 }
 int catfood(int &r) {
-	//고양이 먹이 요금에 합친후 추가주문 할지 선택
-	return 0; //일단은
+		return 0; 
 }
 bool customer_Interface(string &c) {
 	bool lp2 = true;
@@ -19,8 +16,8 @@ bool customer_Interface(string &c) {
 	char a = 0;
 	string pass;
 	while (lp2) {
-		cout << "안녕하세요, 고양이 카페에 오신것을 환영합니다." << endl << "아래 번호중 하나를 선택하세요." << endl;
-		cout << "1. 입장료" << endl << "2.커피" << endl << "3.고양이 용품" << endl << endl << endl << endl << endl << "*. 관리자 모드";
+		cout << "Hellow, welcome to cat cafe." << endl << "Please select one of the following numbers. << endl;
+		cout << "1.Admission Fees " << endl << "2.coffee" << endl << "3.cat item" << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl <<"*. manager mode";
 		cin >> a;
 		switch (a) {
 		case '1':
@@ -47,9 +44,9 @@ bool customer_Interface(string &c) {
 			else
 				break;
 			
-			// 입력받은 값과 password값을 비교해서 맞을 경우 return boolean 할수 있다.
+
 		default:
-			cout << "다시 입력 해주세요";
+			cout << "Please re-enter";
 		}
 		
 	}
